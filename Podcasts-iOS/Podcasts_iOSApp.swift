51731f7fb19ae057bf0aa847c47fbec68b18441d
Shawn Roller <sroller@techstyle.com>
2021-06-15 09:15:09 -0400

    Initial Commit

---
+++ Podcasts-iOS/Podcasts_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Podcasts_iOSApp.swift
+//  Podcasts-iOS
+//
+//  Created by Shawn Roller on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Podcasts_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
