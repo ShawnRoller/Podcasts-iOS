c0ee5648ff69a5a30d2bee697d68bd0271a29910
Shawn Roller <sroller@techstyle.com>
2021-06-15 09:43:40 -0400

    Adds project and large podcast view.

---
+++ Podcasts-iOS/Components/PodcastLargeView.swift
@@ -0,0 +1,36 @@
+//
+//  PodcastLargeView.swift
+//  Podcasts-iOS
+//
+//  Created by Shawn Roller on 6/15/21.
+//
+
+import SwiftUI
+
+struct PodcastLargeView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            Image("podcast1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(8)
+                .padding(.bottom, 10)
+            Text("NEW • MONDAY")
+                .font(.caption2)
+                .foregroundColor(.gray)
+            Text("#82: Avoiding the Buzz Saw (Bret Weinstein & Heather Heying DarkHorse Livestream)")
+                .font(.body)
+                .lineLimit(2)
+            Text("3 Newer Episodes")
+                .font(.subheadline)
+                .foregroundColor(.purple)
+        }
+        .padding()
+    }
+}
+
+struct PodcastLargeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PodcastLargeView()
+    }
+}
