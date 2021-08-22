0d39f2e71d3b53fca4b1cf661f9c365b10d204d8
Shawn Roller <sroller@techstyle.com>
2021-08-22 11:21:32 -0400

    Fleshes out thumbnail section view.

--- Podcasts-iOS/Components/ThumbnailSection.swift
+++ Podcasts-iOS/Components/ThumbnailSection.swift
@@ -8,15 +8,28 @@
 import SwiftUI
 
 struct ThumbnailSection: View {
-    var podcasts: [PodcastEpisode]
+    var podcasts: [Podcast]
+    var size: ThumbnailSize = .Large
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(podcasts) { podcast in
+                    ForEach(podcast.episodes) { episode in
+                        PodcastView(podcast: podcast, podcastEpisode: episode, size: size)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ThumbnailSection_Previews: PreviewProvider {
     static var previews: some View {
-        ThumbnailSection(podcasts: [])
+        Group {
+            ThumbnailSection(podcasts: [Podcast.example, Podcast.example], size: .Large)
+            ThumbnailSection(podcasts: [Podcast.example, Podcast.example], size: .Medium)
+            ThumbnailSection(podcasts: [Podcast.example, Podcast.example], size: .Small)
+        }
     }
 }
